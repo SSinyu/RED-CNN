7c41280bc64b85270699e70b1e7867b720710128
JEONG SINYU <sinyu@ds.seoultech.ac.kr>
2018-08-06 10:05:24 +0900

    Update Mayo_REDCNN_train.py

--- Mayo/Mayo_REDCNN_train.py
+++ Mayo/Mayo_REDCNN_train.py
@@ -1,41 +1,57 @@
 import os
+import pickle
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from Mayo_REDCNN_dataloader import DCMsDataset2
+from Mayo_REDCNN_dataloader import train_dcm_data_loader, validate_dcm_data_loader
 from Mayo_REDCNN_model import RED_CNN
+from skimage.measure import compare_mse, compare_psnr, compare_ssim
 
-def update_lr(optimizer, lr):
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
+LEARNING_RATE = 1e-4
+NUM_EPOCHS = 1000
+BATCH_SIZE = 5
+PATCH_SIZE = 55
+CROP_NUMBER = 40
+NUM_WORKERS = 30
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def main():
-    ### full img training
+    # train data
     full_data_path = '/home/shsy0404/Mayo-CT-full/'
     full_data_dir = sorted(os.listdir(full_data_path))
     input_img_dir = full_data_path + full_data_dir[1]
     target_img_dir = full_data_path + full_data_dir[3]
 
-    input_dir = []
+    input_dir = []; target_dir = []
     for f in sorted(os.listdir(input_img_dir)):
         input_dir.append(np.load(input_img_dir + '/' + f))
-    target_dir = []
     for f in sorted(os.listdir(target_img_dir)):
         target_dir.append(np.load(target_img_dir + '/' + f))
 
-    LEARNING_RATE = 1e-4
-    NUM_EPOCHS = 1000
-    BATCH_SIZE = 10
-    OUT_CHANNELS = 96
+    # validation data
+    val_data_path = '/home/shsy0404/Mayo-CT-full/test/'
+    input_val_dir_ = sorted([data for data in os.listdir(val_data_path) if 'input' in data])
+    target_val_dir_ = sorted(list(set(os.listdir(val_data_path)) - set(input_val_dir_)))
 
-    dcm = DCMsDataset2(input_dir, target_dir, crop_size=55, crop_n=10)
-    dcm_loader = DataLoader(dcm, batch_size=BATCH_SIZE, shuffle=True, num_workers=30)
+    input_val_dir = []; target_val_dir = []
+    for (inp, tar) in zip(input_val_dir_[-11:], target_val_dir_[-11:]):
+        inp_ = np.load(val_data_path + inp)
+        inp_ = np.float32(inp_)
+        tar_ = np.load(val_data_path + tar)
+        tar_ = np.float32(tar_)
+        input_val_dir.append(inp_)
+        target_val_dir.append(tar_)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # train, validate loader
+    train_dcm = train_dcm_data_loader(input_dir, target_dir, crop_size=PATCH_SIZE, crop_n=CROP_NUMBER)
+    train_loader = DataLoader(train_dcm, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 
-    red_cnn = RED_CNN(OUT_CHANNELS)
+    val_dcm = validate_dcm_data_loader(input_val_dir, target_val_dir)
+    val_loader = DataLoader(val_dcm, batch_size=1, num_workers=NUM_WORKERS)
 
+    red_cnn = RED_CNN()
     if torch.cuda.device_count() > 1:
         print("Use {} GPUs".format(torch.cuda.device_count()), "=" * 10)
         red_cnn = nn.DataParallel(red_cnn)
@@ -44,32 +60,86 @@ def main():
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(red_cnn.parameters(), lr=LEARNING_RATE)
 
-    total_step = len(dcm_loader)
-    current_lr = LEARNING_RATE
-
+    loss_ = []; rmse = []; psnr = []; ssim = []
     for epoch in range(NUM_EPOCHS):
-        for i, (inputs, targets) in enumerate(dcm_loader):
-            inputs = inputs.reshape(-1, 55, 55).to(device)
-            targets = targets.reshape(-1, 55, 55).to(device)
-            input_img = torch.tensor(inputs, requires_grad=True, dtype=torch.float32).unsqueeze(1).to(device)
-            target_img = torch.tensor(targets, dtype=torch.float32).unsqueeze(1).to(device)
 
-            outputs = red_cnn(input_img)
-            loss = criterion(outputs, target_img)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        loss_lst = train(train_loader, red_cnn, criterion, optimizer, epoch)
+        loss_.extend(loss_lst)
 
-            if (i + 1) % 10 == 0:
-                print('EPOCH [{}/{}], STEP [{}/{}], LOSS {:.4f}'.format(epoch + 1, NUM_EPOCHS, i + 1, total_step, loss.item()))
+        r, p, s = validate(val_loader, red_cnn)
+        rmse.append(r)
+        psnr.append(p)
+        ssim.append(s)
 
-            if (epoch + 1) % 100 == 0:
-                torch.save(red_cnn.state_dict(), '/home/shsy0404/red_cnn_mayo_patch_{}ep.ckpt'.format(epoch + 1))
+        if (epoch + 1) % 100 == 0:
+            torch.save(red_cnn.state_dict(), '/home/shsy0404/red_cnn_mayo_patch_{}ep.ckpt'.format(epoch + 1))
 
-                current_lr /= 1.1
-                update_lr(optimizer, current_lr)
+    result_ = {'Loss':loss_, 'RMSE':rmse, 'PSNR':psnr, 'SSIM':ssim}
+    with open('home/shsy0404/result_.pkl', 'wb') as f:
+        pickle.dump(result_, f)
+
+
+
+def train(data_loader, model, criterion, optimizer, epoch):
+    model.train()
+    loss_lst = []
+    for i, (inputs, targets) in enumerate(data_loader):
+        inputs = inputs.reshape(-1, 55, 55).to(device)
+        targets = targets.reshape(-1, 55, 55).to(device)
+        input_img = torch.tensor(inputs, requires_grad=True, dtype=torch.float32).unsqueeze(1).to(device)
+        target_img = torch.tensor(targets, dtype=torch.float32).unsqueeze(1).to(device)
+
+        outputs = model(input_img)
+        loss = criterion(outputs, target_img)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        loss_lst.append(loss.item())
+
+        if (i + 1) % 10 == 0:
+            print('EPOCH [{}/{}], STEP [{}/{}], LOSS {:.4f}'.format(epoch + 1, NUM_EPOCHS, i + 1, len(data_loader), loss.item()))
+    return loss_lst
+
+
+def validate(data_loader, model):
+    model.eval()
+
+    with torch.no_grad():
+        rmse_lst = []; psnr_lst = []; ssim_lst = []
+
+        for i, (inputs, targets) in enumerate(data_loader):
+            input_img = inputs.to(device).unsqueeze(0)
+            target_img = targets.to(device)
+
+            output_img = model(input_img)
+            output_img = output_img.squeeze(0).squeeze(0)
+            output_img = output_img.data.cpu().numpy()
+
+            target_img = target_img.squeeze(0)
+            target_img = target_img.data.cpu().numpy()
+
+            rmse_lst.append(np.sqrt(compare_mse(output_img, target_img)))
+            psnr_lst.append(compare_psnr(output_img, target_img, data_range=4096))
+            ssim_lst.append(compare_ssim(output_img, target_img, data_range=4096))
+
+        rmse_avg = np.mean(rmse_lst)
+        psnr_avg = np.mean(psnr_lst)
+        ssim_avg = np.mean(ssim_lst)
+        print('RMSE [{:.4f}], PSNR [{:.4f}], SSIM [{:.4f}]'.format(
+            rmse_avg, psnr_avg, ssim_avg))
+
+    return rmse_avg, psnr_avg, ssim_avg
+
+
+def adjust_learning_rate(optimizer, epoch):
+    lr = LEARNING_RATE * (0.1 ** (epoch // 100))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
 
 
 if __name__ == "__main__":
     main()
+
 
