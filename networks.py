ec7d10df6c8a7eb679c6f4a0785fae9b99af0049
SSinyu <shsy0404g@gmail.com>
2020-11-17 11:53:37 +0900

    Fix Networks

--- networks.py
+++ networks.py
@@ -5,31 +5,38 @@ import torch.nn as nn
 class RED_CNN(nn.Module):
     def __init__(self, out_ch=96):
         super(RED_CNN, self).__init__()
-        self.conv_first = nn.Conv2d(1, out_ch, kernel_size=5, stride=1, padding=0)
-        self.conv = nn.Conv2d(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
-        self.conv_t = nn.ConvTranspose2d(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
-        self.conv_t_last = nn.ConvTranspose2d(out_ch, 1, kernel_size=5, stride=1, padding=0)
+        self.conv1 = nn.Conv2D(1, out_ch, kernel_size=5, stride=1, padding=0)
+        self.conv2 = nn.Conv2D(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+        self.conv3 = nn.Conv2D(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+        self.conv4 = nn.Conv2D(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+        self.conv5 = nn.Conv2D(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+
+        self.tconv1 = nn.ConvTranspose2d(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+        self.tconv2 = nn.ConvTranspose2d(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+        self.tconv3 = nn.ConvTranspose2d(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+        self.tconv4 = nn.ConvTranspose2d(out_ch, out_ch, kernel_size=5, stride=1, padding=0)
+        self.tconv5 = nn.ConvTranspose2d(out_ch, 1, kernel_size=5, stride=1, padding=0)
+
         self.relu = nn.ReLU()
 
     def forward(self, x):
         # encoder
-        residual_1 = x.clone()
-        out = self.relu(self.conv_first(x))
-        out = self.relu(self.conv(out))
-        residual_2 = out.clone()
-        out = self.relu(self.conv(out))
-        out = self.relu(self.conv(out))
-        residual_3 = out.clone()
-        out = self.relu(self.conv(out))
-
+        residual_1 = x
+        out = self.relu(self.conv1(x))
+        out = self.relu(self.conv2(out))
+        residual_2 = out
+        out = self.relu(self.conv3(out))
+        out = self.relu(self.conv4(out))
+        residual_3 = out
+        out = self.relu(self.conv5(out))
         # decoder
-        out = self.conv_t(out)
+        out = self.tconv1(out)
         out += residual_3
-        out = self.conv_t(self.relu(out))
-        out = self.conv_t(self.relu(out))
+        out = self.tconv2(self.relu(out))
+        out = self.tconv3(self.relu(out))
         out += residual_2
-        out = self.conv_t(self.relu(out))
-        out = self.conv_t_last(self.relu(out))
+        out = self.tconv4(self.relu(out))
+        out = self.tconv5(self.relu(out))
         out += residual_1
         out = self.relu(out)
         return out
